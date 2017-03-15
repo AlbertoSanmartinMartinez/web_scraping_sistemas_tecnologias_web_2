dc7f52e43b1e2a8ca30dbcaee11a98a6e2acd001
Alberto Sanmartin Martinez <albertosanmartinmartinez@MacBook-Pro-de-Alberto.local>
2017-03-15 21:39:13 +0100

    terminado

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # web_scraping_sistemas_tecnologias_web_2
 # aplicacion que te muestra la prevision del tiempo
-# 
+# el argumentos de entrada tiene que ser la api_key en el caso que el programa no la incluya
+# he intentado probar otras ciudades (p.e: zaragoza) y se complica ya que existen hasta 8 ciudades con el mismo nombre en todo el mundo
--- file.py
+++ file.py
@@ -16,17 +16,13 @@ extrae los datos de la API y devuelve un prediccion del tiempo
 class WeatherInfo(object):
     '''clase principal para informacion meteorologica'''
 
-    def __init__(self):
-        '''constructor de la clase WeatherInfo'''
-        self.forecast = self.buildMeteoData()
-
     def methodAlmanac(self):
         '''metodo que devuelve el historial del tiempo'''
         return self.getWeb(self.buildUrl(services["almanac"]))
 
     def methodHourly(self):
         '''metodo que devuelve la prevision del tiempo'''
-        return self.getWeb(self.buildUrl("/hourly/q/CA/"))
+        return self.getWeb(self.buildUrl(services["hourly"]))
 
     def getWeb(self, url_web):
         '''metodo que descarga la web'''
@@ -34,7 +30,7 @@ class WeatherInfo(object):
         return self.getDictionary(req)
 
     def getDictionary(self, req):
-        '''metodo que obtiene el diccionario a partir del xml'''
+        '''metodo que obtiene el diccionario a partir del json'''
         return json.loads(req.text)
 
     def buildUrl(self, function):
@@ -42,97 +38,92 @@ class WeatherInfo(object):
         url_root = "http://api.wunderground.com/api/"
         return url_root + api_key + function + location + ".json"
 
-    def printWeatherInfo(self):
-        '''metodo que imprime la prevision del tiempo'''
-        for element in self.forecast:
-            print element
-            print "\n"
-
     def buildMeteoData(self):
         '''metodo que construye la prevision del tiempo'''
-        forecast = []
         almanac_info = self.extractInfoAlmanac()
         hourly_info = self.extractInfoHourly()
 
         current_date = time.strftime("%d-%m-%Y")
-        current_time = time.strftime("%H:%M")
+        print "Weather info for " + current_date + " in " + location.title()
 
-        forecast.append("La prevision del tiempo para el: "
-                        + current_date + " a las: " + current_time + " es:")
-        forecast.append(almanac_info)
-        forecast.append(hourly_info)
+        tips = []
 
-        # calculos de la prevision de tiempo
-        return forecast
+        self.printTips(tips)
+        self.printHourly(hourly_info)
+        self.printAlmanac(almanac_info)
 
     def extractInfoAlmanac(self):
         '''metodo que sirve para extraer la informacion
            del diccionario almanac'''
-        info = {}
+        l = []
         almanac_dict = self.methodAlmanac()
+        aux = almanac_dict['almanac']['temp_high']['normal']['C']
+        l.append("Aaverage maximum temperature: " + aux + "ºC".decode("utf-8"))
+        aux = almanac_dict['almanac']['temp_high']['record']['C']
+        l.append("Maximum temperature record: " + aux + "ºC".decode("utf-8"))
+        aux = almanac_dict['almanac']['temp_high']['recordyear']
+        l.append("Record year: " + aux)
+        aux = almanac_dict['almanac']['temp_low']['normal']['C']
+        l.append("Average minimum temperature: " + aux + "ºC".decode("utf-8"))
+        aux = almanac_dict['almanac']['temp_low']['record']['C']
+        l.append("Minimum temperaturerecord: " + aux + "ºC".decode("utf-8"))
+        aux = almanac_dict['almanac']['temp_low']['recordyear']
+        l.append("Record year: " + aux)
 
-        info['temp_high'] = {}
-        info['temp_high']['average'] = \
-            almanac_dict['almanac']['temp_high']['normal']['C']
-        info['temp_high']['record'] = \
-            almanac_dict['almanac']['temp_high']['record']['C']
-        info['temp_high']['year'] = \
-            almanac_dict['almanac']['temp_high']['recordyear']
-
-        info['temp_low'] = {}
-        info['temp_low']['average'] = \
-            almanac_dict['almanac']['temp_low']['normal']['C']
-        info['temp_low']['record'] = \
-            almanac_dict['almanac']['temp_low']['record']['C']
-        info['temp_low']['year'] = \
-            almanac_dict['almanac']['temp_low']['recordyear']
-
-        return info
+        return l
 
     def extractInfoHourly(self):
         '''metodo que sirve para extraer la informacion
            del diccionario hourly'''
-        result = []
+        l = []
         hourly_dict = self.methodHourly()
         hourly_dict = hourly_dict["hourly_forecast"]
 
         for item in hourly_dict:
             info = []
-            # hour = item["FCTTIME"]["pretty"].decode("utf-8")
-            info.append("la prevision para las: " + item["FCTTIME"]
-                        ["pretty"].decode("utf-8") + " es:")
-            # condition = item['condition']
-            info.append("The weather is: " + item['condition'])
-            # thermal_sensation = item['feelslike']['metric']
-            info.append("The thermal condition is: " +
-                        item['feelslike']['metric'])
-            # snow_cover = item['snow']['metric']
-            info.append("The snow cover is: " + item['snow']['metric'])
-            # temp = item['temp']['metric']
-            info.append("The temperature is: " + item['temp']['metric'])
-            # wind_speed = item['wspd']['metric']
-            info.append("The wind speed is: " + item['wspd']['metric'])
-            # humidity = item['humidity']
-            info.append("The humidity is: " + item['humidity'])
-            # pressure = item['mslp']['metric']
-            info.append("The pressure is: " + item['mslp']['metric'])
-            result.append(info)
+            info.append("Weather information for " + item["FCTTIME"]
+                        ["pretty"] + " :")
 
-        return result
+            print item["FCTTIME"]["pretty"]
+
+            info.append("Condition: " + item['condition'])
+            info.append("Thermal condition: " +
+                        item['feelslike']['metric'])
+            info.append("Snow cover: " + item['snow']['metric'])
+            info.append("Temperature: " + item['temp']['metric'])
+            info.append("Wind speed: " + item['wspd']['metric'])
+            info.append("Humidity: " + item['humidity'])
+            info.append("Pressure: " + item['mslp']['metric'])
+            l.append(info)
+
+        return l
+
+    def printAlmanac(self, almanac_info):
+        print "Historical weather information:"
+        for item in almanac_info:
+            print item
+
+    def printHourly(self, hourly_info):
+        for item in hourly_info:
+            print item
+            for element in item:
+                print element
+            print "\n"
+
+    def printTips(self, tips):
+        for item in tips:
+            print item
 
 
 if __name__ == "__main__":
     '''programa princiapl'''
 
     api_key = "5020486343d0db43"
-    location = None
+    location = "Lleida"
     services = {"almanac": "/almanac/q/CA/", "hourly": "/hourly/q/CA/"}
 
     if api_key is None:
         api_key = sys.argv[1]
-        location = sys.argv[2]
-    else:
-        location = sys.argv[1]
 
     weather_info = WeatherInfo()
-    weather_info.printWeatherInfo()
+    weather_info.buildMeteoData()
